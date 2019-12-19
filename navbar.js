b1173366b80a99ff2bffcf0e3aef8c31c1715aa0
Cranky Supertoon <48419040+CrankySupertoon@users.noreply.github.com>
2019-12-18 19:51:53 -0500

    fix toadette

--- navbar.js
+++ navbar.js
@@ -335,6 +335,8 @@ function updateNvChar () { //actually use this now
 			addNvChar('toad');
 			addNvChar('boo');
 			addNvChar('koopakid');
+			addNvChar('toadette');
+
 			break;
 		case 'mp7':
 			addNvChar('mario');
